77c926a815c2c8665e36e12fa37c0a46826a2eb1
Jacqueline Linang <jacquelinang@Jacquelines-MacBook-Pro-4.local>
2016-09-08 11:41:21 -0400

    v1
    Selfie Project for ARS 327

---
+++ js/main.js
@@ -0,0 +1,80 @@
+$(".card-1").flip({
+    axis: 'y',
+    trigger: 'click'
+});
+
+$(".card-2").flip({
+    axis: 'y',
+    trigger: 'click'
+});
+
+$(".card-3").flip({
+    axis: 'y',
+    trigger: 'click'
+});
+
+$(".card-4").flip({
+    axis: 'y',
+    trigger: 'click'
+});
+
+$(".card-5").flip({
+    axis: 'y',
+    trigger: 'click'
+});
+
+$(".card-6").flip({
+    axis: 'y',
+    trigger: 'click'
+});
+
+$(".card-7").flip({
+    axis: 'y',
+    trigger: 'click'
+});
+
+$(".card-8").flip({
+    axis: 'y',
+    trigger: 'click'
+});
+
+$(".card-9").flip({
+    axis: 'y',
+    trigger: 'click'
+});
+
+$('.card-1').click(function(){
+   document.body.className = "bg-1"; 
+});
+
+$('.card-2').click(function(){
+   document.body.className = "bg-2"; 
+});
+
+$('.card-3').click(function(){
+   document.body.className = "bg-3"; 
+});
+
+$('.card-4').click(function(){
+   document.body.className = "bg-4"; 
+});
+
+$('.card-5').click(function(){
+   document.body.className = "bg-5"; 
+});
+
+$('.card-6').click(function(){
+   document.body.className = "bg-6"; 
+});
+
+$('.card-7').click(function(){
+   document.body.className = "bg-7"; 
+});
+
+$('.card-8').click(function(){
+   document.body.className = "bg-8"; 
+});
+
+$('.card-9').click(function(){
+   document.body.className = "bg-9"; 
+});
